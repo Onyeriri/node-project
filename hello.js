2e9da8a9fd39da044245b9af9e972f79c5ab5a11
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-19 01:04:18 +0000

    Initial commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+let mission = process.argv[2]
+
+if(mission === 'learning') {
+  console.log('Learning node again hope i finish this time guys');
+}else {
+  console.log(`Let's ${mission} this node for real brother's`)
+}

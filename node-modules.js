63c77ceda3b832d5f168700b81029e065b82b0d9
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-19 14:52:53 +0000

    Using get function

--- node-modules.js
+++ node-modules.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const { get } = require('http');
 
-const request = http.request('http://www.google.com', (res) => {
+get('http://www.google.com', (res) => {
   res.on('data', (chunk) => console.log(chunk));
   res.on('end', () => console.log('No more data'))
 });
 
-request.end();
+// app.end();

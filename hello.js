4f53116cf625c9ff93be266825863a657fc3a29d
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-19 02:30:24 +0000

    Deep dive into event emitters in node

--- hello.js
+++ hello.js
@@ -1,7 +1,24 @@
-let mission = process.argv[2]
+// let mission = process.argv[2]
 
-if(mission === 'learning') {
-  console.log('Learning node again hope i finish this time guys');
-}else {
-  console.log(`Let's ${mission} this node for real brother's`)
-}
+// if(mission === 'learning') {
+//   console.log('Learning node again hope i finish this time guys');
+// }else {
+//   console.log(`Let's ${mission} this node for real brother's`)
+// }
+
+const EventEmitter = require('events');
+// const exit = require('exit');
+
+
+const celebrity = new EventEmitter();
+
+// observer one
+celebrity.on('race win', () => console.log('You win'));
+
+// observer 2
+celebrity.on('race win', () => console.log('Boo, loser'));
+
+process.on('exit', (code) => console.log(code));
+
+celebrity.emit('race win', () => console.log('You win'));
+celebrity.emit('race win', () => console.log('Boo, loser'));

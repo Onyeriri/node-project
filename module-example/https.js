bad11d130d43efe68f29ed94cebd261da2fc2aca
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-20 23:12:42 +0000

    Import using commonJS

--- module-example/https.js
+++ module-example/https.js
@@ -1,7 +1,7 @@
 // using Ecmascript syntax
 
-import send from './request';
-import read from './response';
+import send from './request.mjs';
+import read from './response.mjs';
 
 function request(url, data) {
   send(url, data)

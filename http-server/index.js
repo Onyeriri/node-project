321045d9e4de1f8f4e6ec247940b134cad37e655
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-22 23:29:18 +0000

    Using different routes to make a request

--- http-server/index.js
+++ http-server/index.js
@@ -2,15 +2,29 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
+const server = http.createServer();
 
-  res.end('Hello node server');
-});
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.write('<h1>Hello node server</h1>');
+    res.end();
+  } else if (req.url === '/message') {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.write('<h1>message from node server</h1>');
+    res.end();
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 400;
+    res.write('<h6>Page not found from node server</h6>');
+    res.end();
+  }
+})
 
 
 server.listen(PORT, () => {
   console.log(`Server started and listening to ${PORT}`)
-})
+});
+

a38c6098ad5c2a6a2da0d25e47d6c7c3dd05305a
Onyeriri Victor <viktorchinonso@gmail.com>
2023-02-22 11:52:38 +0000

    Added new script command to package manager

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-// axios.get('http://www.google.com')
-//   .then((response => console.log(response)))
+// // axios.get('http://www.google.com')
+// //   .then((response => console.log(response)))
 
 
-const getResponse = async () => {
-  try {
-    const response = await axios.get('http://www.facebook.com')
+// const getResponse = async () => {
+//   try {
+//     const response = await axios.get('http://www.facebook.com')
 
-    console.log(response)
+//     console.log(response)
 
-  } catch (error) {
-    console.log(error)
-  }
-};
+//   } catch (error) {
+//     console.log(error)
+//   }
+// };
 
-getResponse()
+// getResponse()
+
+const parse = require('csv-parse');
+const fs = require('fs');
+
+const data = [];
+
+fs.createReadStream('./plant_project/kepler_data.csv')
+  .on('data', (chunk) => {
+  data.push(chunk)
+  })
+  .on('error', (error) => {
+  console.log(error)
+  })
+  .on('end', () => {
+  console.log('End of file')
+  })
+
+// console.log(data)
+// parse();
